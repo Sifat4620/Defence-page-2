0126fe2f21b7e63a0311760438f93c3f855c64ca
Fahim Faisal Sifat <81096504+Sifat4620@users.noreply.github.com>
2022-03-22 23:28:19 +0600

    Add files via upload

---
+++ toxic mafia 2.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <link rel="SHORTCUT ICON" href="https://i.imgur.com/mEwlVW6.png" type="image/x-icon"/>
+        <title>ᵀᵒˣⁱᶜ ★𝐌𝐀𝐅𝐈𝐀★</title>
+		<meta name="Author" content="Smurfy"/>
+		<meta name="copyright" content="Royal Battler BD Smurfy"/>
+		<script> alert('HACKED BY SMURFY') </script>
+    </head>
+    <body>
+	   <style type="text/css">body{background-image:url('https://i.imgur.com/tj5ygVT.jpg');background-color:#000000;background-repeat:no-repeat;background-position:right top;color:#ffffff;margin:0px;}</style>
+	   <center>
+       <img src="https://i.imgur.com/mEwlVW6.png" width="400"/>
+       <div align="center">
+       <div class="a"><p align="center"><b> Hacked By Smurfy</p></b></div>
+       <br />
+	   <font size="3" color="#FFFFFF">
+Im just here to inform you that i have just Found a Vulnerability on your security<br>
+Please try to Improve your Website Security. I'm not the only Hacker in the world.<br>
+You dont even know what they can do or what are they going to do<br>
+Once they got access to your site and before  Black Hat Hacker's is coming and Destroy all your Database information..<br>
+Or files Compromised, all your files Deleted, all your files Coppied And Malicious Files Planted..<br>
+Please Patch your Security..<br>
+<br>
+We are Legion<br>
+We do not Forgive<br>
+We do not Forget<br>
+Stand as One<br>
+Divided by Null<br>
+Expect Us !!<br>
+<br>
+<br>
+-=[ GREETINGS and RESPECT ]=-
+      <font/>
+       <br />
+       <br />
+      <div class="d"> <marquee><font color="silver"> Fighter Anas ~ 3rr0r Hun73r ~ Dev19feb ~ Smurfy ~ M4d K!ll3r ~ VandaTheGod ~ CY83R_!Nj3C70R And All Others Of Royal Battler BD</font></marquee></span><br></div>
+ 
+Contact us: https://www.facebook.com/s6669/
+	   <STYLE type=text/css>
+	   .a {
+    font-size:35.5px;
+    color:red;
+    text-shadow:1px 1px 0.5em white;
+}
+body {
+    background-color:black;
+}
+.b{
+    font-style:italic;
+    color:red;
+ 
+}
+.c {
+    color:green;
+}
+.d{
+    font-variant:bold;
+    color:red;
+    text-shadow:1px 1px 1px red;
+}
+	   </STYLE>
+	   <iframe width="1" height="1" src="https://www.youtube.com/embed/eTj1Fhfgouk?t=6?rel=0&t=0m5s&autoplay=1&loop=1&playlist=eTj1Fhfgouk?t=6" frameborder="0" allowfullscreen=""></iframe>
+	 </body>
+</html>
